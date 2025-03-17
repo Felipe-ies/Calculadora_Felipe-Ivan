a4c91e40e8e34345659dad1180ee0d1b0bb69a95
A31P25\dam <ivan080906@gmail.com>
2025-03-17 20:11:24 +0100

    añadido metodo resta

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -5,6 +5,10 @@ public class Calculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inicio del proyecto colaborativo");
+		
 	}
+	public static int resta(int a, int b) {
+		 return a - b;
+		}
 
 }
